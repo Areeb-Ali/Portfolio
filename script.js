7053db6f612b56ca7a3b2089761a2ac8bef36864
Areeb-Ali <areebithink@gmail.com>
2024-03-17 00:43:35 +0500

    About Section Completed

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-let tabLinks = document.querySelector('.tab-links')
+let tabLinks = document.querySelector(".tab-links");
+let tabContents = document.querySelector(".tab-contents");
+
+function openTab(tabName) {
+  for (tabLink of tabLinks) {
+    tabLink.classList.remove("active-link");
+  }
+  for (tabContent of tabContents) {
+    tabContent.classList.remove("active-tab");
+  }
+  Event.currentTarget.classList.add("active-link");
+  document.getElementById(tabName).classList.add('active-tab')
+}
